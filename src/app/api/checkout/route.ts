55224467e59b6293b9cf7ca157502cba097d53b4
qtmarketingweb-creator <qtmarketingweb@gmail.com>
2026-04-26 15:02:11 -0600

    Fix Stripe API version

--- src/app/api/checkout/route.ts
+++ src/app/api/checkout/route.ts
@@ -1,13 +1,14 @@
 import Stripe from 'stripe'
 import { NextRequest, NextResponse } from 'next/server'
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, { apiVersion: '2024-06-20' })
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string)
 export async function POST(req: NextRequest) {
   try {
     const { email } = await req.json()
     const session = await stripe.checkout.sessions.create({
       mode: 'subscription',
       payment_method_types: ['card'],
-      line_items: [{ price: process.env.STRIPE_PRICE_ID!, quantity: 1 }],
+      line_items: [{ price: process.env.STRIPE_PRICE_ID as string, quantity: 1 }],
       success_url: process.env.NEXT_PUBLIC_URL + '/success?session_id={CHECKOUT_SESSION_ID}',
       cancel_url: process.env.NEXT_PUBLIC_URL + '/',
       customer_email: email || undefined,
