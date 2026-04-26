feac36c09c52fdfd8039b3efd9b73f86fb57de6b
qtmarketingweb-creator <qtmarketingweb@gmail.com>
2026-04-26 01:21:59 -0600

    Add tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,3 @@
 import type { Config } from 'tailwindcss'
-const config: Config = {
-  content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
-  theme: { extend: {} },
-  plugins: [],
-}
+const config: Config = { content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'], theme: { extend: {} }, plugins: [] }
 export default config
